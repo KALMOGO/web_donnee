736a8dcd16da908f1b9d8df473ae86c28f085916
kalmogo <lucienkalmogo21@gmail.com>
2023-04-26 03:48:56 +0200

    end_menu_footer

--- unb/universite/urls.py
+++ unb/universite/urls.py
@@ -3,5 +3,18 @@ from .views import *
 
 
 urlpatterns = [
-    path("", Home_PageView, name='home')
+    path("", Home_PageView, name='home'),
+    path("actualite/", Actualite_PageView, name='actualite'),
+    path("actualite/<int:id>/detail", Actualite_Detail_PageView, name='_actualite_detail'),
+    path("formation/", Formation_PageView, name='formation'),
+    path("etablissement/home/", Etablissement_PageView, name='etablissement_home'),
+    path("etablissement/", Home_etablissement_PageView, name='etablissement'),
+    path("laboratoire/", Laboratoire_PageView, name='laboratoire'),
+    path("partenaire/", Partenaire_PageView, name='partenaire'),
+    path("site/", Site_PageView, name='site'),
+    path("ecole_doctorale/", EcoleDoctorale_PageView, name='ecole_doctorale'),
+    path("ecole_doctorale_home/", Home_ecole_doctorale_PageView, name='ecole_doctorale_home'),
+    path("document/", Document_PageView, name='document'),
+    path("presentation/", Presentation_PageView, name='presentation'),
+    path("recherche/", Recherche_PageView, name="recherche")
 ]
--- unb/universite/views.py
+++ unb/universite/views.py
@@ -7,3 +7,63 @@ def Home_PageView(request):
     context = {"page_title": "Home Page"}
     
     return render(request, "index.html", context=context)
+
+def Presentation_PageView(request):
+    context={}
+    return render(request, "presentation.html", context=context)
+
+def Recherche_PageView(request):
+    context={}
+    return render(request, "recherche.html", context=context)
+
+def Actualite_PageView(request):
+    
+    context = {}
+    return render(request, "actualite.html", context=context)
+
+def Formation_PageView(request):
+    context = {}
+    return render(request, "formation.html", context=context)
+
+
+def Etablissement_PageView(request):
+    context = {}
+    return render(request, "etablissement.html", context=context)
+
+
+def Home_etablissement_PageView(request):
+    context = {}
+    return render(request, "home_etablissement.html", context=context)
+
+
+def Laboratoire_PageView(request):
+    context = {}
+    return render(request, "laboratoire.html", context=context)
+
+
+def Partenaire_PageView(request):
+    context = {}
+    return render(request, "partenaire.html", context=context)
+
+
+def Site_PageView(request):
+    context = {}
+    return render(request, "site.html", context=context)
+
+
+def EcoleDoctorale_PageView(request):
+    context = {}
+    return render(request, "ecole_doctorale.html", context=context)
+
+def Home_ecole_doctorale_PageView(request):
+    context = {}
+    return render(request, "home_ecole_doctorale.html", context=context)
+
+def Document_PageView(request):
+    context = {}
+    return render(request, "document.html", context=context)
+
+
+def Actualite_Detail_PageView(request):
+    context = {}
+    return render(request, "actualite_detail.html", context=context)
